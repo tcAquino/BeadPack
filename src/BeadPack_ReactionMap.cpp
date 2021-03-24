02b25738c722b1965a885361b2e74de6351e2043
Tomás Aquino <tomasaquino@Tomass-MacBook-Pro.local>
2021-03-24 13:28:29 +0100

    Fixed signed-unsigned comparisons

--- src/BeadPack_ReactionMap.cpp
+++ src/BeadPack_ReactionMap.cpp
@@ -88,7 +88,7 @@ int main(int argc, const char * argv[])
   using JumpGenerator_Diffusion = ctrw::JumpGenerator_Diffusion;
   using JumpGenerator = ctrw::JumpGenerator_Add<JumpGenerator_Advection, JumpGenerator_Diffusion>;
 
-  std::size_t arg = 1;
+  int arg = 1;
   double damkohler = atof(argv[arg++]);
   double peclet = atof(argv[arg++]);
   double time_step_accuracy_adv = atof(argv[arg++]);
